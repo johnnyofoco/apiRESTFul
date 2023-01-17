4d5f83bcc3d23655f311d90df0b4dbffb8a5ab75
johnnyofoco <johnny.marlon.paula@gmail.com>
2023-01-17 10:04:15 -0300

    adicionando método delete de products

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,54 +2,69 @@ const ProductsModel = require("../models/productsModel");
 
 async function get(req, res) {
   const { id } = req.params;
-  
+
   // se tiver id atribuiu o id ao obj caso contrário envia null
   // assim o find() irá funcionar pra retornar todos os itens
   // como tbm apenas um único item
-  const obj = id ? { _id: id} : null
+  const obj = id ? { _id: id } : null;
 
   const products = await ProductsModel.find(obj);
 
   res.send(products);
 }
 
-async function post(req, res){
-    const {name, brand, price,} =  req.body
+async function post(req, res) {
+  const { name, brand, price } = req.body;
 
-    const product = new ProductsModel({
-        name,
-        brand,
-        price,
-    })
+  const product = new ProductsModel({
+    name,
+    brand,
+    price,
+  });
 
-    product.save()
+  product.save();
 
-    res.send({
-        message: 'Product registred'
-    })
+  res.send({
+    message: "Product registred",
+  });
 }
 
-async function put(req, res){
-    const {id} = req.params
-    
-    //atualiza porém retorna o produto atualizado na resposta
-    const product = await ProductsModel.findOneAndUpdate({_id: id}, req.body, { new: true})
-    
-    res.send({
-        message: "product updated",
-        product,
-    }) 
+async function put(req, res) {
+  const { id } = req.params;
 
-    // Metódo que atualiza porém retorna o produto antigo na resposta
- /*
+  //atualiza porém retorna o produto atualizado na resposta
+  const product = await ProductsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+  });
+
+  res.send({
+    message: "product updated",
+    product,
+  });
+
+  // Metódo que atualiza porém retorna o produto antigo na resposta
+  /*
     const product = await ProductsModel.findOne({_id: id})
      await product.updateOne(req.body)
-*/  
-    
+*/
+}
+
+async function remove(req, res) {
+  const { id } = req.params;
+
+  const remove = await ProductsModel.deleteOne({ _id: id });
+
+  const message = remove ? "product removed" : "error";
+
+  res.send({
+    message,
+    id,
+  });
 }
 
 module.exports = {
   get,
   post,
   put,
+  remove,
 };
