96d33869579ca5f6d8b8ac9aabdcdac2d3644923
johnnyofoco <johnny.marlon.paula@gmail.com>
2023-01-17 10:51:03 -0300

    adicionando middleware de CORS na API

--- src/server.js
+++ src/server.js
@@ -1,20 +1,41 @@
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
 // const path = require('path')
 
-const db = require('./database/db')
-const routes = require('./routes/routes')
+const db = require("./database/db");
+const routes = require("./routes/routes");
 
-const app = express()
+const app = express();
 
 // conexão com o banco de dados
-db.connect()
+db.connect();
+
+// origens permitidas a consumir a API
+const allowedOrigins = ["http://127.0.0.1:5501", "http://www.app.com.br"];
+
+// habilta CORS (cors() torna a API pública, disponível a todos os dominios)
+// app.use(cors())
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      let allowed = true;
+
+      //permitir acesso via mobile app (sem origem)
+      if (!origin || origin === null) allowed = true;
+
+      if (!allowedOrigins.includes(origin)) allowed = false;
+
+      callback(null, allowed);
+    },
+  })
+);
 
 // habilita server para receber dados no formato JSON
-app.use(express.json())
+app.use(express.json());
 
 //definindo as rotas
-app.use('/api', routes)
+app.use("/api", routes);
 
 //executando o servidor
-const port = process.env.PORT || 3000
-app.listen(port, ()=> console.log(`Server is listening on port ${port}...`))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
