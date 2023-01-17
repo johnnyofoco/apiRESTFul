72319901cf9539b67cb3b7a01198ffb13bd8e404
johnnyofoco <johnny.marlon.paula@gmail.com>
2023-01-17 09:21:09 -0300

    adicionando metodo get de products

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,11 +1,18 @@
-const ProductsModel = require('../models/productsModel')
+const ProductsModel = require("../models/productsModel");
 
-async function get(req, res){
-    const products = await ProductsModel.find()
+async function get(req, res) {
+  const { id } = req.params;
+  
+  // se tiver id atribuiu o id ao obj caso contrário envia null
+  // assim o find() irá funcionar pra retornar todos os itens
+  // como tbm apenas um único item
+  const obj = id ? { _id: id} : null
 
-    res.send(products)
+  const products = await ProductsModel.find(obj);
+
+  res.send(products);
 }
 
 module.exports = {
-    get,
-}
+  get,
+};
